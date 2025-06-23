f303733ba28b3118d12feae6dbced03dc6d9a850
Nelly <no222hh@student.lnu.se>
2025-06-23 21:30:05 +0200

    added icons

--- my-react-app/src/components/ContactSection.js
+++ my-react-app/src/components/ContactSection.js
@@ -1,20 +1,38 @@
 import './ContactSection.css';
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
 
 const ContactSection = () => {
-    return (
-        <section className="contact">
-            <div className="overlay-text">Hamna i balans</div>
-            <img src="images/pexels-rdne-6724357.jpg" alt="Kontakt"  />
-            <div className="overlay-text2">Någon text här...... </div>
-           
-            
-            <h2 className="section-title">Kontakt</h2>
-            <p>Har du några frågor eller vill du boka en tid? Tveka inte att höra av dig!</p>
-            <p><strong>Telefon:</strong>  0735162722</p>
-            <p><strong>Email: </strong>annlis.hedman@live.se </p>
-            <p><strong>Adress:</strong> Källavägen 9, 394 77, Kalmar</p>
-        </section>
-    );
+  return (
+    <section className="contact">
+      <div className="overlay-text">Hamna i balans</div>
+      <img src="images/pexels-rdne-6724357.jpg" alt="Kontakt" />
+      <div className="overlay-text2">Någon text här...... </div>
+
+      <h2 className="section-title">Kontakt</h2>
+      <p>Har du några frågor eller vill du boka en tid? Tveka inte att höra av dig!</p>
+
+      <p >
+        <FaPhoneAlt className="contact-icon" />
+        <a href="tel:0735162722">0735162722</a>
+      </p>
+
+      <p>
+        <FaEnvelope className="contact-icon" />
+        <a href="mailto:annlis.hedman@live.se">annlis.hedman@live.se</a>
+      </p>
+
+      <p>
+        <FaMapMarkerAlt className="contact-icon" />
+        <a
+          href="https://www.google.com/maps/search/?api=1&query=Källavägen+9,+394+77,+Kalmar"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Källavägen 9, 394 77, Kalmar
+        </a>
+      </p>
+    </section>
+  );
 };
 
 export default ContactSection;
